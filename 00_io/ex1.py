2b7cd7fc5bd2920bd0fd1f86332f801e598512db
hyeonjin123456789 <hyeonjin0428@dimigo.hs.kr>
2026-08-21 11:50:07 +0900

    add 01_datatype ex1.py ex2.py

--- 00_io/ex1.py
+++ 00_io/ex1.py
@@ -1,34 +1,34 @@
-# ÀÔÃâ·Â
+# ì…ì¶œë ¥
 
-a = input()
-print(a, end="")
-print(type(a))
-print(a, type(a), sep=",")
+# a = input()
+# print(a, end="")
+# print(type(a))
+# print(a, type(a), sep=",")
 
-a = int(a)
-print(a, type(a))
+# a = int(a)
+# print(a, type(a))
 
-a = int(input())
-print(a , type(a))
+# a = int(input())
+# print(a , type(a))
 
-b = float(input())
-print(b, type(b))
+# b = float(input())
+# print(b, type(b))
 
-# Á¤¼ö 2°³ ÀÔ·Â
-# 100
-# 200
-a = int(input())
-b = int(input())
-print(a, b)
+# # ì •ìˆ˜ 2ê°œ ì…ë ¥
+# # 100
+# # 200
+# a = int(input())
+# b = int(input())
+# print(a, b)
 
-# 100 200
-a = input().split()
-print(a , type(a))
+# # 100 200
+# a = input().split()
+# print(a , type(a))
 
-# map
-a, b, c = map(int, input().split())
-print(a,b,c)
+# # map
+# a, b, c = map(int, input().split())
+# print(a,b,c)
 
-# ¸®½ºÆ® º¯È¯
+# ë¦¬ìŠ¤íŠ¸ ë³€í™˜
 a = list(map(int, input().split()))
-print(a, type(a))
+print(a, type(a)) 
